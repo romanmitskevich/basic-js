508810d69deb5d67bbb66ec0c4d3e1c31c59c63e
roman.mits <roman.mitskevich1@mail.ru>
2022-05-02 14:26:48 +0300

    feat: add solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+   let arrayWithNumbers = String(n).split('').map(element => Number(element));
+   let resultArray = [];
+
+   arrayWithNumbers.map(element => {
+      let temporaryArray = [...arrayWithNumbers];
+      temporaryArray.splice(temporaryArray.indexOf(element), 1);
+
+      resultArray.push(temporaryArray);
+   });
+
+   return Math.max(...resultArray.map(element => element.join('')));
 }
 
 module.exports = {
-  deleteDigit
+   deleteDigit
 };
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+   let resultObject = {};
+   let arrayWithSplittedArgs = domains.map(el => el.split(''));
+   let arrWithIndexes = [];     // массив с индексами точек
+   let array = [];              // массив со всеми DNS, но для каждого элемента domains он обнуляется
+   let result = [];             // массив cо всеми DNS
+
+
+   arrayWithSplittedArgs.map(item => {
+      for (let i = 0; i < item.length; i++) {
+         if (item[i] === '.') {
+            arrWithIndexes.push(i);
+         }
+      }
+
+      for (let i = arrWithIndexes.length - 1; i >= 0; i--) {
+         array.push(item.slice(arrWithIndexes[i]).join(''));
+         item.splice(arrWithIndexes[i], item.length - arrWithIndexes[i]);
+      }
+
+      array.push(`.${item.join('')}`);
+
+      for (let i = 1; i < array.length; i++) {
+         if (array[i - 1]) {
+            array[i] = array[i - 1] + array[i];
+         }
+      }
+
+      array.map(element => result.push(element));
+      arrWithIndexes = [];
+      array = [];
+   })
+
+   let count = 0;
+   result.map(element => {
+      for (let i = 0; i < result.length; i++) {
+         if (element === result[i]) {
+            count++;
+         }
+      }
+
+      resultObject[element] = count;
+      count = 0;
+   })
+
+   return resultObject;
 }
 
 module.exports = {
-  getDNSStats
+   getDNSStats
 };
